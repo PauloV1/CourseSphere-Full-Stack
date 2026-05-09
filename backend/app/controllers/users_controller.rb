15dbf1d6053900e109bade0661f4a182d652f82b
Paulo Vitor <pv5350812@gmail.com>
2026-05-09 19:52:30 -0300

    feat(auth): add name field and nest user data in json responses

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -1,15 +1,29 @@
 class UsersController < ApplicationController
   skip_before_action :authorize_request, only: [:create]
+  before_action :authorize_request, only: [:me]
 
   def me
-    render json: { user: @current_user }, status: :ok
+    render json: { 
+      user: { 
+        id: @current_user.id, 
+        email: @current_user.email, 
+        name: @current_user.name 
+      } 
+    }, status: :ok
   end
 
   def create
     user = User.new(user_params)
     if user.save
       token = JsonWebToken.encode(user_id: user.id)
-      render json: { token: token, email: user.email }, status: :created
+      render json: { 
+        token: token,
+        user: { 
+          id: user.id,
+          email: user.email, 
+          name: user.name 
+        } 
+      }, status: :created
     else
       render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
     end
@@ -17,6 +31,6 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.permit(:email, :password, :password_confirmation)
+    params.permit(:name, :email, :password, :password_confirmation)
   end
 end
